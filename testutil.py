bb745c0b8d94a5b210229cf74c8399330b31264f
MarloDelatorre <mtorre0157@gmail.com>
2020-01-16 14:26:58 -0800

    Added a slightly better error message for assert_matrix_equal

--- testutil.py
+++ testutil.py
@@ -3,12 +3,13 @@ from unittest import TestCase
 
 class MatrixTestCase(TestCase):
     def assert_matrix_equal(self, A, B):
+        error_message = f"\nactual: {A}, \nexpected: {B}"
         self.assertEqual(len(A), len(B))
         self.assertEqual(len(A[0]), len(B[0]))
 
         for row in range(len(A)):
             for col in range(len(A[0])):
-                self.assertEqual(A[row][col], B[row][col])
+                self.assertEqual(A[row][col], B[row][col], msg=error_message)
 
 class ListOfListsTestCase(TestCase):
     def assert_list_of_lists_equal(self, list1: List[List], list2: List[List]): 
